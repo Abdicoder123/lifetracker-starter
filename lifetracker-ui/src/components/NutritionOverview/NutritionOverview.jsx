fd266a6790c1bebb36b3a85a56993c6630e40df1
Abdirahman Ali <abdirahman.ali@abdirah-ltm4tx2.internal.salesforce.com>
2022-07-15 17:16:33 -0700

    Last commit

--- lifetracker-ui/src/components/NutritionOverview/NutritionOverview.jsx
+++ lifetracker-ui/src/components/NutritionOverview/NutritionOverview.jsx
@@ -3,24 +3,25 @@ import { useNutritionContext } from "../../contexts/nutrition";
 import NutritionFeed from "components/NutritionFeed/NutritionFeed";
 import { Link } from "react-router-dom";
 
-export default function NutritionOverview(){
-    const { nutrition, error, isInitialized, isProcessing, logNutrition } = useNutritionContext();
-    
-    if(error){
-        return (
-            <h2 className="error">Error...</h2>
-        )
-    } 
- 
-    return (
-        <div className="nutrition-overview">
-            <h1 className="overview-title">Nutrition</h1>
-            <div className="nutrition-wrapper">
-                <h2 className="nutrition-title">Overview</h2>
-                <Link to="/nutrition/create"><button className="create-Nutrition">Record Nutrition</button></Link>
-            </div>
-            <NutritionFeed nutrition={nutrition}/>
-        </div>
-    )
+export default function NutritionOverview() {
+  const { nutrition, error, isInitialized, isProcessing, logNutrition } =
+    useNutritionContext();
 
+  if (error) {
+    return <h2 className="error">Error...</h2>;
+  }
+
+  return (
+    <div className="nutrition-overview">
+      <h1 className="overview-title">Nutrition</h1>
+      <div className="nutrition-wrapper">
+        <h2 className="nutrition-title">Overview</h2>
+        <Link to="/nutrition/create">
+          <button className="create-Nutrition">Record Nutrition</button>
+        </Link>
+      </div>
+      <NutritionFeed nutrition={nutrition} />
+    </div>
+  );
 }
+// See if it works online
--- lifetracker-ui/src/contexts/activity.jsx
+++ lifetracker-ui/src/contexts/activity.jsx
@@ -1,62 +1,74 @@
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React from "react";
 import ApiClient from "../services/apiClient";
-import { useContext } from 'react';
+import { useContext } from "react";
 const ActivityContext = React.createContext(null);
 
-export const  ActivityContextProvider = ({ children }) => {
-    const [activity, setActivity] = React.useState({});
-    const [initialized, setInitial] = React.useState(false);
-    const [isLoading, setIsLoading] = React.useState(true);
-    const [error, setError] = React.useState(null);
-    const [exerciseStats, setExerciseStats] = React.useState(null);
-    const [showSleep, setShowSleep] = React.useState(true);
-    const [sleep, logSleep] = React.useState(0);
-    
-    React.useEffect(async () => {
-        setIsLoading(true);
-        if(localStorage.getItem("lifetracker_token")){
-            ApiClient.setToken(localStorage.getItem("lifetracker_token"));
-          }
-        try {
-            const req = await ApiClient.getActivity();
-            setActivity(req.data);
-        }
-        catch(err){
-            console.log(err);
-        }
-        setIsLoading(false);
-        setInitial(true);
-    },[])
+// The Activity react java script file.
+export const ActivityContextProvider = ({ children }) => {
+  const [activity, setActivity] = React.useState({});
+  const [initialized, setInitial] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+  const [exerciseStats, setExerciseStats] = React.useState(null);
+  const [showSleep, setShowSleep] = React.useState(true);
+  const [sleep, logSleep] = React.useState(0);
 
-    React.useEffect(async () => {
-        setIsLoading(true);
-        if(localStorage.getItem("lifetracker_token")){
-            ApiClient.setToken(localStorage.getItem("lifetracker_token"));
-          }
-        try {
-            const req = await ApiClient.getActivityExercise();
-            setExerciseStats(req.data);
-        }
-        catch(err){
-            console.log(err);
-        }
-        setIsLoading(false);
-        setInitial(true);
-    },[])
-    
-    const reload = () => {
-        if(isLoading === false){
-            window.location.reload();
-        }
+  React.useEffect(async () => {
+    setIsLoading(true);
+    if (localStorage.getItem("lifetracker_token")) {
+      ApiClient.setToken(localStorage.getItem("lifetracker_token"));
     }
+    try {
+      const req = await ApiClient.getActivity();
+      setActivity(req.data);
+    } catch (err) {
+      console.log(err);
+    }
+    setIsLoading(false);
+    setInitial(true);
+  }, []);
 
-    const activityValue = {activity, setActivity, isLoading, setIsLoading, initialized, setInitial, reload, exerciseStats, showSleep, setShowSleep, sleep, logSleep };
-    return (
-        <ActivityContext.Provider value={activityValue}>
-            {children}
-        </ActivityContext.Provider>
-    )
-}
+  React.useEffect(async () => {
+    setIsLoading(true);
+    if (localStorage.getItem("lifetracker_token")) {
+      ApiClient.setToken(localStorage.getItem("lifetracker_token"));
+    }
+    try {
+      const req = await ApiClient.getActivityExercise();
+      setExerciseStats(req.data);
+    } catch (err) {
+      console.log(err);
+    }
+    setIsLoading(false);
+    setInitial(true);
+  }, []);
+
+  const reload = () => {
+    if (isLoading === false) {
+      window.location.reload();
+    }
+  };
+
+  const activityValue = {
+    activity,
+    setActivity,
+    isLoading,
+    setIsLoading,
+    initialized,
+    setInitial,
+    reload,
+    exerciseStats,
+    showSleep,
+    setShowSleep,
+    sleep,
+    logSleep,
+  };
+  return (
+    <ActivityContext.Provider value={activityValue}>
+      {children}
+    </ActivityContext.Provider>
+  );
+};
 
 export const useActivityContext = () => useContext(ActivityContext);
