b3efce5e1062be3d99bc4c9d94abe0ff22a69baf
Abdirahman Ali <abdirahman.ali@abdirah-ltm4tx2.internal.salesforce.com>
2022-07-15 17:15:31 -0700

    Last commit

--- README.md
+++ README.md
@@ -20,10 +20,10 @@ The login is a bit wonky since I set it up
 - [x] **The Landing Page:** Display a large hero image and a brief blurb on what this application is about
 - [x] **Login Page:** A form that allows users to login with email and password.
 - [x] **Registration Page:** A form that allows the user to sign up with their email, password, username, first name, and last name.
-- [ ] When a user first authenticates, they should be redirected to an authenticated view (i.e the detailed activity page). When they sign out, all frontend data should be reset.
-- [ ] Users have access to an overview Activity page that show one summary statistic about each of the 3 types of activity tracked.
+- [x] When a user first authenticates, they should be redirected to an authenticated view (i.e the detailed activity page). When they sign out, all frontend data should be reset.
+- [x] Users have access to an overview Activity page that show one summary statistic about each of the 3 types of activity tracked.
 - [x] The API should have a `security` middleware that only allows authenticated users to access resources and only allows users to access resources about themselves.
-- [ ] Users should have the ability to track at least **1** types of activities (i.e Nutrition, Exercise, Sleep, etc.). Each activity should be tracked on separate pages.
+- [x] Users should have the ability to track at least **1** types of activities (i.e Nutrition, Exercise, Sleep, etc.). Each activity should be tracked on separate pages.
 - [x] Deployed website with Heroku & Surge.
 
 **Detailed Activity Page:**
--- lifetracker-ui/src/services/apiClient.js
+++ lifetracker-ui/src/services/apiClient.js
@@ -1,89 +1,102 @@
-import axios from "axios"
+import axios from "axios";
 import React from "react";
 import constants from "constants";
 
 console.log(constants);
 
 class ApiClient {
-    constructor(remoteHostUrl){
-        this.remoteHostUrl = remoteHostUrl;
-        this.token = null;
-        this.tokenName = "lifetracker_token"
-    }
+  constructor(remoteHostUrl) {
+    this.remoteHostUrl = remoteHostUrl;
+    this.token = null;
+    this.tokenName = "lifetracker_token";
+  }
 
-    //Sets the token
-    async setToken(token){
-        this.token = token;
-        localStorage.setItem(this.tokenName, this.token);
-    }
+  //Sets the token
+  async setToken(token) {
+    this.token = token;
+    localStorage.setItem(this.tokenName, this.token);
+  }
 
-    async removeToken(){
-        this.token = null;
-        localStorage.removeItem(this.tokenName);
-    }
+  async removeToken() {
+    this.token = null;
+    localStorage.removeItem(this.tokenName);
+  }
 
-     reload() {
-        window.location.reload();
-        return false;
-    }
+  reload() {
+    window.location.reload();
+    return false;
+  }
 
-    //Issues axios requests
-    async request({endpoint, method = "GET", data = {}}){
-        const url = `${this.remoteHostUrl}/${endpoint}`
+  //Issues axios requests
+  async request({ endpoint, method = "GET", data = {} }) {
+    const url = `${this.remoteHostUrl}/${endpoint}`;
 
-        const headers = {
-            "Content-Type": "application/json"
-        }
-        if(this.token){
-            headers["Authorization"] = `Bearer ${this.token}`
-        }
-        try {
-            const request = await axios({url, method, data, headers});
-            if(request){ 
-                return {data: request.data, error:null};
-            }
-        }
-        catch(err){
-            console.log(err)
-            return {data:null, error:err};
-        }
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    if (this.token) {
+      headers["Authorization"] = `Bearer ${this.token}`;
     }
+    try {
+      const request = await axios({ url, method, data, headers });
+      if (request) {
+        return { data: request.data, error: null };
+      }
+    } catch (err) {
+      console.log(err);
+      return { data: null, error: err };
+    }
+  }
 
-    async fetchUserFromToken(){
-        return await this.request({ endpoint: `auth/me`, method: `GET` })
-    }
+  async fetchUserFromToken() {
+    return await this.request({ endpoint: `auth/me`, method: `GET` });
+  }
 
-    //Sends a request to Auth/Login
-     async login(loginData){
-        return await this.request({endpoint: `auth/login`, method:`POST`, data:loginData})
-    }
+  //Sends a request to Auth/Login
+  async login(loginData) {
+    return await this.request({
+      endpoint: `auth/login`,
+      method: `POST`,
+      data: loginData,
+    });
+  }
 
-   async signup(loginData){   
-    return await this.request({endpoint: `auth/register`, method:`POST`, data:loginData})
-    }
+  async signup(loginData) {
+    return await this.request({
+      endpoint: `auth/register`,
+      method: `POST`,
+      data: loginData,
+    });
+  }
 
-    async logNutrition(nutritionData){
-        return await this.request({endpoint: `nutrition`, method:`POST`, data: nutritionData})
-    }
-    async getNutrition(){
-        return await this.request({ endpoint: `nutrition`, method: `GET` })
-    }
+  async logNutrition(nutritionData) {
+    return await this.request({
+      endpoint: `nutrition`,
+      method: `POST`,
+      data: nutritionData,
+    });
+  }
+  async getNutrition() {
+    return await this.request({ endpoint: `nutrition`, method: `GET` });
+  }
 
-    async getActivity(){
-        return await this.request({ endpoint: `activity`, method: `GET` })
-    }
-    async getActivityExercise(){
-        return await this.request({ endpoint: `activity/exercise`, method: `GET` })
-    }
+  async getActivity() {
+    return await this.request({ endpoint: `activity`, method: `GET` });
+  }
+  async getActivityExercise() {
+    return await this.request({ endpoint: `activity/exercise`, method: `GET` });
+  }
 
-    async getExercises(){
-        return await this.request({ endpoint: `exercise`, method: `GET` })
-    }
-    async logExercises(exerciseData){
-        return await this.request({endpoint: `exercise`, method:`POST`, data: exerciseData})
-    }
-} 
+  async getExercises() {
+    return await this.request({ endpoint: `exercise`, method: `GET` });
+  }
+  async logExercises(exerciseData) {
+    return await this.request({
+      endpoint: `exercise`,
+      method: `POST`,
+      data: exerciseData,
+    });
+  }
+}
 
-
-export default new ApiClient("https://lifetracker-site-amaar.herokuapp.com");
-
+export default new ApiClient("https://lifetracker-aaali-starter.herokuapp.com");
